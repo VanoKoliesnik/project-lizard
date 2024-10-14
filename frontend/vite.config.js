e76ffd118126575b0d010a3f42f2fd2caa4f02dc
Ivan Koliesnik <ikoliesnik@de.fi>
2024-10-14 14:09:58 +0200

    chore: use nodeResolve

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
-import resolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default defineConfig({
   plugins: [
     react(),
-    resolve({
+    nodeResolve({
       extensions: ["ts", "tsx"],
     }),
   ],
