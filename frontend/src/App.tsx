26b75ad9ecce00b22ac9db70a2abc989a88f857e
Ivan Koliesnik <kolesnikivan1002@gmail.com>
2024-10-15 11:09:15 +0200

    chore: moved pages to components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,9 +9,9 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { Loading, PageContainer, PrivateRoute } from "./components";
 import { RoutesPaths } from "./common/enums";
-import { RegistrationPage } from "./pages/Registration";
-import { LoginPage } from "./pages/Login";
-import { ProfilePage } from "./pages/Profile";
+import { RegistrationPage } from "./components/Registration";
+import { LoginPage } from "./components/Login";
+import { ProfilePage } from "./components/Profile";
 
 function App() {
   return (
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import alias from "@rollup/plugin-alias";
 import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     react(),
-    nodeResolve({
-      extensions: ["ts", "tsx"],
-      browser: true,
-    }),
     alias({
       entries: [
         {
